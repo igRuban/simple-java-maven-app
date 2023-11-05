288077af6237564be4007d01621664556037edf2
Ruban <126575183+igRuban@users.noreply.github.com>
2023-11-05 14:57:16 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,12 @@
 package com.mycompany.app;
 
 /**
- * Helloworld  !   
+ * Hello world  !   
  */ 
 public class App
 {
 
-    private final String message  = "Hello World!";
+    private final String message  = "Hello  World!";
 
     public App() {}
 
